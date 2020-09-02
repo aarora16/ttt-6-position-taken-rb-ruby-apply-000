09ad81ac007795601dc41a21ec7f233fa755708f
Anshul Arora <a.99.anshul@gmail.com>
2020-09-02 18:35:41 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,11 +1,9 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
+  
   if board[index] == " " || ""
     false
   else
-    true
-  end
-  when board[index] == "X" || "O"
     true
   end
 end
